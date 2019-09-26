2942cb16bc58028494ef1c8c422b514b718d896f
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:17:57 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan8-pecahan/src/pboulang/pkg10115109/latihan8/pecahan/Pboulang10115109Latihan8Pecahan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan8.pecahan;
+
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float :" + f);
+        System.out.println("double :" + d);
+        
+    }
+    
+}
